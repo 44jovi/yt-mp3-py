58ba7e954c61589cb70bcecaed0d9a5335b62d7d
jovi <kwong.jovi@gmail.com>
2023-01-04 15:41:41 +0000

    refactor(app-basic): auto-format and update comment

--- app-basic.py
+++ app-basic.py
@@ -1,16 +1,19 @@
 import youtube_dl
 
-# Basic core function of converting YouTube video to .mp3 format
+# Basic version with only core function of converting YouTube video to .mp3 format
+
+
 def download_audio(youtube_url):
-  yt_dl_options = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-      }],
-  }
-  with youtube_dl.YoutubeDL(yt_dl_options) as ydl:
-    ydl.download([youtube_url])
+    yt_dl_options = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(yt_dl_options) as ydl:
+        ydl.download([youtube_url])
+
 
 download_audio("https://youtu.be/OBLkvpBHLkc")
