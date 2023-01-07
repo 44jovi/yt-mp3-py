c43c75aee0e25d0ba5d708fe74b2642959278296
jovi <kwong.jovi@gmail.com>
2023-01-07 23:47:47 +0000

    feat(app): add function to return most recently crreated mp3 in directory

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import youtube_dl
+import glob
+import os
 
 
 def download_audio(youtube_url):
@@ -12,6 +14,11 @@ def download_audio(youtube_url):
     }
     with youtube_dl.YoutubeDL(yt_dl_options) as yt_dl:
         yt_dl.download([youtube_url])
+
+
+def latest_mp3_filename():
+    mp3s_list = glob.glob('./*.mp3')
+    return max(mp3s_list, key=os.path.getctime)
 
 
 def main():
